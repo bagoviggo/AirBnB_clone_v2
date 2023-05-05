7748f386e54fd2d8c93ba89736a3cf84cacbbfaf
bagoviggo <bagoviggo@gmail.com>
2023-05-05 23:46:56 +0300

    Fix error task 2

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,39 +1,30 @@
 #!/usr/bin/python3
 """
-Fabric script to distribute an archive to your web servers
+Fabric script based on the file 1-pack_web_static.py that distributes an
+archive to the web servers
 """
-from fabric.api import env, put, run
+
+from fabric.api import put, run, env
 from os.path import exists
-import os.path
-import re
-
-
 env.hosts = ['54.242.191.75', '100.26.212.141']
 
 
 def do_deploy(archive_path):
-    """
-    Deploy the web static files
-    """
-    if not os.path.exists(archive_path):
+    """distributes an archive to the web servers"""
+    if exists(archive_path) is False:
         return False
     try:
-        put(archive_path, "/tmp/")
-        file_name = archive_path.split("/")[-1]
-        folder_name = re.sub('.tgz$', '', file_name)
-        run("sudo mkdir -p /data/web_static/releases/{}".format(folder_name))
-        run("sudo tar -xzf /tmp/{} -C /data/web_static/releases/{}/"
-            .format(file_name, folder_name))
-        run("sudo rm /tmp/{}".format(file_name))
-        run("sudo mv /data/web_static/releases/{}/web_static/* "
-            "/data/web_static/releases/{}/".format(folder_name, folder_name))
-        run("sudo rm -rf /data/web_static/releases/{}/web_static"
-            .format(folder_name))
-        run("sudo rm -rf /data/web_static/current")
-        run("sudo ln -s /data/web_static/releases/{}/ "
-            "/data/web_static/current".format(folder_name))
+        file_n = archive_path.split("/")[-1]
+        no_ext = file_n.split(".")[0]
+        path = "/data/web_static/releases/"
+        put(archive_path, '/tmp/')
+        run('mkdir -p {}{}/'.format(path, no_ext))
+        run('tar -xzf /tmp/{} -C {}{}/'.format(file_n, path, no_ext))
+        run('rm /tmp/{}'.format(file_n))
+        run('mv {0}{1}/web_static/* {0}{1}/'.format(path, no_ext))
+        run('rm -rf {}{}/web_static'.format(path, no_ext))
+        run('rm -rf /data/web_static/current')
+        run('ln -s {}{}/ /data/web_static/current'.format(path, no_ext))
         return True
-    except Exception as e:
-        print("Error: {}".format(e))
+    except:
         return False
-
